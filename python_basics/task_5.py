9d5c3446adacd5078491447095d57fcba1df895c
axreldable <starikovaleksei1992@mail.ru>
2019-04-22 20:17:56 +0300

    task_5 - tryings

--- python_basics/task_5.py
+++ python_basics/task_5.py
@@ -2,26 +2,58 @@
 # add <namespace> <var>
 # get <namespace> <var>
 
+
 main_dict = {
-    "global": {
-        "foo": {
-            "vars": {
-                "a": 1,
-                "b": 1,
-            }
-        },
-        "bar": {
-            "vars": {
-                "a": 1,
-                "b": 1,
-            }
-        },
+    'global': {
+        'parent': None,
+        'vars': set('a'),
+        'foo': {
+            'parent': 'global',
+            'vars': set('b'),
+            'bar': {
+                'parent': 'foo',
+                'vars': set('a')}
+        }
     }
 }
 
-def crate(namespace, parent):
-    for  main_dict["global"].keys()
+dict1 = {
+    'global': {
+        'parent': None,
+        'vars': set()
+    }
+}
+
+cur_dict = dict1['global']
 
 
-def get_namespace(cur_dict, name):
-    return cur_dict[name]
+def create(namespace, parent):
+    create_d(dict1, namespace, parent)
+
+
+def create_d(dic, namespace, parent):
+    if dic[parent][namespace] is None:
+        dic[parent][namespace] = {
+            'parent': parent,
+            'vars': set()
+        }
+    else:
+        for key in dic.keys():
+            create_d(dic[key], namespace, parent)
+
+
+def add(namespace, var):
+    cur_dict[namespace]['vars'].add(var)
+
+
+# def get(namespace, var):
+#     return cur_dict[name]
+
+print(cur_dict)
+create('foo', 'global')
+print(cur_dict)
+add('foo', 'a')
+print(cur_dict)
+create('bar', 'foo')
+print(cur_dict)
+
